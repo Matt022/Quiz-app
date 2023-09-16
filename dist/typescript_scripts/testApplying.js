19f4d136de779b450b7c9e50c2d6b8dd712054c7
Matus Benus <matusbenus02@gmail.com>
2023-09-16 13:47:37 +0200

    pridané dátové typy

--- dist/typescript_scripts/testApplying.js
+++ dist/typescript_scripts/testApplying.js
@@ -1,26 +1,40 @@
-import { getTestById } from "../typescript_scripts/dbService.js";
+// Importujeme potrebné moduly
+import { getTestById } from "./dbService.js";
+// Načítame test s ID 0 pomocou funkcie getTestById
 getTestById(0).then((test) => {
     // Funkcia na generovanie otázok a odpovedí z JSON
     function generateQuestionsAndAnswers() {
+        // Uložíme načítaný test do premennej testHere
         const testHere = test;
+        // Získame odkaz na HTML formulár s id "test-form"
         const form = document.getElementById('test-form');
+        // Skontrolujeme, či sme načítali platný test
         if (testHere) {
             const correctAnswers = []; // Pole pre uchovávanie informácií o správnych odpovediach
+            // Prejdeme všetky otázky v teste
             for (let i = 0; i < testHere.otazky.length; i++) {
+                // Vytvoríme kontajner pre otázku
                 const questionContainer = document.createElement('div');
                 questionContainer.classList.add('question-container');
+                // Vytvoríme značku pre otázku
                 const questionText = document.createElement('label');
                 questionText.textContent = `${i + 1}. ${testHere.otazky[i].text}`;
                 questionContainer.appendChild(questionText);
+                // Prejdeme všetky odpovede na otázku
                 for (let j = 0; j < testHere.otazky[i].odpovede.length; j++) {
+                    // Vytvoríme značku pre odpoveď a radio tlačidlo
                     const answerLabel = document.createElement('label');
                     const radioInput = document.createElement('input');
                     radioInput.type = 'radio';
-                    radioInput.name = `question-${i + 1}`; // Jedinečný názov pre každú otázku
+                    radioInput.name = `question-${i}`; // Jedinečný názov pre každú otázku
                     radioInput.value = j.toString(); // Použijeme hodnotu indexu odpovede ako hodnotu
                     // Ak je odpoveď správna, pridajte ju do poľa správnych odpovedí
                     if (testHere.otazky[i].odpovede[j].jeSpravna) {
-                        correctAnswers.push({ question: i, answer: j });
+                        const correctAnsw = {
+                            question: i,
+                            answer: j
+                        };
+                        correctAnswers.push(correctAnsw);
                     }
                     answerLabel.appendChild(radioInput);
                     answerLabel.appendChild(document.createTextNode(testHere.otazky[i].odpovede[j].text));
@@ -32,7 +46,7 @@ getTestById(0).then((test) => {
             form.dataset.correctAnswers = JSON.stringify(correctAnswers);
         }
     }
-    // Spustite generovanie otázok pre test s ID 0
+    // Spustíme generovanie otázok pre test s ID 0
     generateQuestionsAndAnswers();
     // Funkcia na výpočet percentuálneho hodnotenia a zobrazenie alertu
     function calculateAndShowResult() {
@@ -41,16 +55,18 @@ getTestById(0).then((test) => {
         let correctAnswers = 0;
         // Získame pole správnych odpovedí zo dátového súboru
         const correctAnswersArray = JSON.parse(form.dataset.correctAnswers || '[]');
+        // Prejdeme všetky otázky a zkontrolujeme správne odpovede
         for (let i = 0; i < totalQuestions; i++) {
             const selectedAnswer = form.querySelector(`input[name="question-${i}"]:checked`);
             if (selectedAnswer && correctAnswersArray.some((answer) => answer.question === i && answer.answer === parseInt(selectedAnswer.value))) {
                 correctAnswers++;
             }
         }
+        // Vypočítame percentuálne hodnotenie a zobrazíme ho v upozornení
         const scorePercentage = (correctAnswers / totalQuestions) * 100;
         alert(`Váš výsledok je ${scorePercentage.toFixed(2)}%`);
     }
-    // Pridajte funkciu pre odoslanie testu na tlačítko
+    // Pridáme funkciu pre odoslanie testu na tlačítko
     const submitButton = document.getElementById('submit-button');
     submitButton.addEventListener('click', calculateAndShowResult);
 });
--- typescript_scripts/testApplying.ts
+++ typescript_scripts/testApplying.ts
@@ -1,34 +1,49 @@
-import { getTestById } from "../typescript_scripts/dbService.js";
-import { Test } from "./test";
+// Importujeme potrebné moduly
+import { getTestById } from "./dbService.js";
+import { Test } from "../typescript_models/test.js";
+import { CorrectAnswer } from '../typescript_models/correctAnswer';
 
+// Načítame test s ID 0 pomocou funkcie getTestById
 getTestById(0).then((test: Test | null) => {
     // Funkcia na generovanie otázok a odpovedí z JSON
     function generateQuestionsAndAnswers(): void {
+        // Uložíme načítaný test do premennej testHere
         const testHere: Test | null = test;
+        // Získame odkaz na HTML formulár s id "test-form"
         const form: HTMLFormElement = document.getElementById('test-form') as HTMLFormElement;
 
+        // Skontrolujeme, či sme načítali platný test
         if (testHere) {
-            const correctAnswers = []; // Pole pre uchovávanie informácií o správnych odpovediach
+            const correctAnswers: CorrectAnswer[] = []; // Pole pre uchovávanie informácií o správnych odpovediach
 
+            // Prejdeme všetky otázky v teste
             for (let i: number = 0; i < testHere.otazky.length; i++) {
+                // Vytvoríme kontajner pre otázku
                 const questionContainer: HTMLDivElement = document.createElement('div');
                 questionContainer.classList.add('question-container');
 
+                // Vytvoríme značku pre otázku
                 const questionText: HTMLLabelElement = document.createElement('label');
                 questionText.textContent = `${i + 1}. ${testHere.otazky[i].text}`;
 
                 questionContainer.appendChild(questionText);
 
+                // Prejdeme všetky odpovede na otázku
                 for (let j: number = 0; j < testHere.otazky[i].odpovede.length; j++) {
+                    // Vytvoríme značku pre odpoveď a radio tlačidlo
                     const answerLabel: HTMLLabelElement = document.createElement('label');
                     const radioInput: HTMLInputElement = document.createElement('input');
                     radioInput.type = 'radio';
-                    radioInput.name = `question-${i + 1}`; // Jedinečný názov pre každú otázku
+                    radioInput.name = `question-${i}`; // Jedinečný názov pre každú otázku
                     radioInput.value = j.toString(); // Použijeme hodnotu indexu odpovede ako hodnotu
 
                     // Ak je odpoveď správna, pridajte ju do poľa správnych odpovedí
                     if (testHere.otazky[i].odpovede[j].jeSpravna) {
-                        correctAnswers.push({ question: i, answer: j });
+                        const correctAnsw: CorrectAnswer = {
+                            question: i,
+                            answer: j
+                        };
+                        correctAnswers.push(correctAnsw);
                     }
 
                     answerLabel.appendChild(radioInput);
@@ -45,7 +60,7 @@ getTestById(0).then((test: Test | null) => {
         }
     }
 
-    // Spustite generovanie otázok pre test s ID 0
+    // Spustíme generovanie otázok pre test s ID 0
     generateQuestionsAndAnswers();
 
     // Funkcia na výpočet percentuálneho hodnotenia a zobrazenie alertu
@@ -57,6 +72,7 @@ getTestById(0).then((test: Test | null) => {
         // Získame pole správnych odpovedí zo dátového súboru
         const correctAnswersArray = JSON.parse(form.dataset.correctAnswers || '[]');
 
+        // Prejdeme všetky otázky a zkontrolujeme správne odpovede
         for (let i: number = 0; i < totalQuestions; i++) {
             const selectedAnswer: HTMLInputElement = <HTMLInputElement>form.querySelector(`input[name="question-${i}"]:checked`);
             if (selectedAnswer && correctAnswersArray.some((answer: any) => answer.question === i && answer.answer === parseInt(selectedAnswer.value))) {
@@ -64,11 +80,12 @@ getTestById(0).then((test: Test | null) => {
             }
         }
 
+        // Vypočítame percentuálne hodnotenie a zobrazíme ho v upozornení
         const scorePercentage: number = (correctAnswers / totalQuestions) * 100;
         alert(`Váš výsledok je ${scorePercentage.toFixed(2)}%`);
     }
 
-    // Pridajte funkciu pre odoslanie testu na tlačítko
+    // Pridáme funkciu pre odoslanie testu na tlačítko
     const submitButton: HTMLButtonElement = document.getElementById('submit-button') as HTMLButtonElement;
     submitButton.addEventListener('click', calculateAndShowResult);
 });
