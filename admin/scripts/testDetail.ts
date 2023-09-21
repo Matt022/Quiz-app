3d502e42f243e3b9dc238160fc9e206f0b619759
Matus Benus <matusbenus02@gmail.com>
2023-09-21 14:34:33 +0200

    menšie zmeny

--- admin/scripts/testDetail.ts
+++ admin/scripts/testDetail.ts
@@ -1,45 +1,60 @@
-import { deleteTest, getTestById, updateTest } from "./dbService.js";
-import { HelperClass } from './helpers.js';
-const helperClass = new HelperClass();
-const testUpdateQuestionsContainer = document.querySelector("div.test-update-questions");
-const deleteTestBtn = document.querySelector("button.deleteTest");
-const editTestBtn = document.querySelector("button.editTest");
-const disableChangesBtn = document.querySelector("button.disableChanges");
-const saveBtn = document.querySelector("#save");
-const addQuestionButton = document.getElementById("add-question");
-const testId = helperClass.getQuizIdFromURL();
+import { Answer } from "../../typescript_models/answer";
+import { Question } from "../../typescript_models/question";
+import { Test } from "../../typescript_models/test";
+import { deleteTest, getTestById, updateTest } from "../../typescript_scripts/dbService.js";
+import { HelperClass } from '../../typescript_scripts/helpers.js';
+
+const helperClass: HelperClass = new HelperClass();
+
+const testUpdateQuestionsContainer: HTMLDivElement = <HTMLDivElement>document.querySelector("div.test-update-questions");
+const deleteTestBtn: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button.deleteTest");
+const editTestBtn: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button.editTest");
+const disableChangesBtn: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button.disableChanges");
+const saveBtn: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#save");
+const addQuestionButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("add-question");
+const testId: number | null = helperClass.getQuizIdFromURL();
+
 if (testId != null) {
-    getTestById(testId).then((test) => {
+    getTestById(testId).then((test: Test | null) => {
         if (test) {
-            const testNameElement = document.getElementById("title");
+            const testNameElement: HTMLSpanElement = <HTMLSpanElement>document.getElementById("title");
             testNameElement.textContent = test.title;
-            const questionsContainer = document.getElementById("questions");
-            for (let i = 0; i < test.questions.length; i++) {
-                const questionDiv = document.createElement("div");
+
+            const questionsContainer: HTMLDivElement = <HTMLDivElement>document.getElementById("questions");
+
+            for (let i: number = 0; i < test.questions.length; i++) {
+                const questionDiv: HTMLDivElement = document.createElement("div");
                 questionDiv.classList.add("question");
-                const questionText = document.createElement("p");
+
+                const questionText: HTMLParagraphElement = document.createElement("p");
                 questionText.classList.add("question-text");
                 questionText.textContent = `Question ${i + 1}: ${test.questions[i].text}`;
-                const answersDiv = document.createElement("div");
+
+                const answersDiv: HTMLDivElement = document.createElement("div");
                 answersDiv.classList.add("answers");
-                test.questions[i].answers.forEach((answer) => {
-                    const answerDiv = document.createElement("div");
+
+                test.questions[i].answers.forEach((answer: Answer) => {
+                    const answerDiv: HTMLDivElement = document.createElement("div");
                     answerDiv.classList.add("answer");
-                    const answerText = document.createElement("p");
+
+                    const answerText: HTMLParagraphElement = document.createElement("p");
                     answerText.textContent = answer.text;
+
                     if (answer.isCorrect) {
                         answerText.classList.add("correct-answer");
-                    }
-                    else {
+                    } else {
                         answerText.classList.add("incorrect-answer");
                     }
+
                     answerDiv.appendChild(answerText);
                     answersDiv.appendChild(answerDiv);
                 });
+
                 questionDiv.appendChild(questionText);
                 questionDiv.appendChild(answersDiv);
                 questionsContainer.appendChild(questionDiv);
             }
+
             deleteTestBtn.addEventListener("click", () => {
                 if (testId != null && confirm("Do you really want to delete this test?")) {
                     deleteTest(testId).then(() => {
@@ -47,22 +62,28 @@ if (testId != null) {
                     });
                 }
             });
+
             editTestBtn.addEventListener("click", () => {
-                const testNameElement = document.getElementById("title");
+                const testNameElement: HTMLSpanElement = <HTMLSpanElement>document.getElementById("title");
                 testNameElement.contentEditable = "true";
-                const questionsDivEl = document.getElementById("questions");
+
+                const questionsDivEl: HTMLDivElement = <HTMLDivElement>document.getElementById("questions");
                 questionsDivEl.style.display = "none";
-                const testUpdateQuestionsContainer = document.querySelector("div.test-update-questions");
+
+                const testUpdateQuestionsContainer: HTMLDivElement = <HTMLDivElement>document.querySelector("div.test-update-questions");
                 testUpdateQuestionsContainer.style.display = "block";
+
                 editTestBtn.style.display = "none";
                 disableChangesBtn.style.display = "inline-block";
                 addQuestionButton.style.display = "inline-block";
                 saveBtn.style.display = "inline-block";
-                for (let i = 0; i < test.questions.length; i++) {
+
+                for (let i: number = 0; i < test.questions.length; i++) {
                     renderQuestionElements(test.questions[i], i);
                 }
+
                 addQuestionButton.addEventListener("click", () => {
-                    const question = {
+                    const question: Question = {
                         text: "",
                         answers: [
                             { text: "", isCorrect: false },
@@ -71,108 +92,140 @@ if (testId != null) {
                             { text: "", isCorrect: false }
                         ],
                     };
+
                     test.questions.push(question);
                     renderQuestionElements(question, test.questions.length - 1);
                 });
+
                 saveBtn.addEventListener("click", () => {
                     // Získáme hodnotu názvu testu zo spanu
-                    const titleInput = document.getElementById("title");
-                    test.title = titleInput.textContent;
+                    const titleInput: HTMLSpanElement = <HTMLSpanElement>document.getElementById("title");
+                    test.title = titleInput.textContent!;
                     test.questions = helperClass.getData("update");
-                    const isAnyQuestionNameEmpty = test.questions.some((question) => question.answers.some((answer) => answer.text === ""));
-                    const isAnyAnswerMarked = test.questions.some((question) => question.answers.every((answer) => answer.isCorrect === false));
+
+                    const isAnyQuestionNameEmpty: boolean = test.questions.some((question: Question) =>
+                        question.answers.some((answer: Answer) => answer.text === "")
+                    );
+
+                    const isAnyAnswerMarked: boolean = test.questions.some((question: Question) =>
+                        question.answers.every((answer: Answer) => answer.isCorrect === false)
+                    );
+
                     if (test.title === "") {
                         alert("Test needs to have a title");
                         return;
                     }
+
                     if (isAnyQuestionNameEmpty) {
                         alert("Complete all questions");
                         return;
                     }
+
                     if (isAnyAnswerMarked) {
                         alert("Mark at least one correct answer");
                         return;
                     }
+
                     updateTest(test.id, test).then(() => {
                         alert("Test was updated.");
                     });
                 });
             });
+
             disableChangesBtn.addEventListener("click", () => {
                 document.location.reload();
             });
-            function renderQuestionElements(question, index) {
+
+            function renderQuestionElements(question: Question, index: number): void {
                 // Vytvoríme kontajnerový div pre otázku
-                const questionDivContainer = document.createElement("div");
+                const questionDivContainer: HTMLDivElement = document.createElement("div");
                 questionDivContainer.className = "question";
+
                 // Vytvoríme label pre označenie otázky
-                const questionLabel = document.createElement("label");
+                const questionLabel: HTMLLabelElement = document.createElement("label");
                 questionLabel.textContent = `Question ${index + 1}:`;
+
                 // Vytvoríme input pre text otázky
-                const questionInput = document.createElement("input");
+                const questionInput: HTMLInputElement = document.createElement("input");
                 questionInput.type = "text";
                 questionInput.value = question.text;
                 questionInput.placeholder = "Question";
                 questionInput.spellcheck = false; // Vypnutie kontrolu pravopisu
+
                 // Pridáme event listener na input, aby sme mohli aktualizovať text otázky v objekte otázky
-                questionInput.addEventListener("input", (e) => {
-                    question.text = e.target.value;
+                questionInput.addEventListener("input", (e: Event) => {
+                    question.text = (e.target as HTMLInputElement).value;
                 });
+
                 // Vytvoríme tlačidlo pre odstránenie otázky
-                const buttonToDelete = document.createElement("button");
+                const buttonToDelete: HTMLButtonElement = document.createElement("button");
                 buttonToDelete.textContent = "Delete";
+
                 // Pridáme event listener na tlačidlo pre odstránenie otázky
                 buttonToDelete.addEventListener("click", () => {
                     if (confirm("Do you really want to delete this question?")) {
                         // Získame kontajner otázok a odstránime aktuálnu otázku z neho
                         questionDivContainer.remove();
-                        test.questions.splice(index, 1); // Odstránime otázku zo zoznamu otázok
+                        test!.questions.splice(index, 1); // Odstránime otázku zo zoznamu otázok
                         updateQuestionNumbers();
                     }
                 });
+
                 // Vytvoríme div pre odpovede
-                const answersDiv = document.createElement("div");
+                const answersDiv: HTMLDivElement = document.createElement("div");
+
                 // Pre každú otázku vytvoríme 4 možné odpovede
-                for (let i = 0; i < 4; i++) {
-                    const answer = question.answers[i];
+                for (let i: number = 0; i < 4; i++) {
+                    const answer: Answer = question.answers[i];
                     const answerDiv = document.createElement("div");
                     answerDiv.className = "answer";
+
                     // Vytvoríme input pre text odpovede
-                    const answerInput = document.createElement("input");
+                    const answerInput: HTMLInputElement = document.createElement("input");
                     answerInput.type = "text";
                     answerInput.placeholder = "Answer";
                     answerInput.value = answer ? answer.text : "";
                     answerInput.spellcheck = false; // Vypnutie kontrolu pravopisu
+
                     // Vytvoríme nový objekt pre odpoveď v každej iterácii
-                    const newAnswer = {
+                    const newAnswer: Answer = {
                         text: "",
                         isCorrect: false,
                     };
+
                     // Vytvoríme checkbox pre označenie správnej odpovede
-                    const correctAnswerInput = document.createElement("input");
+                    const correctAnswerInput: HTMLInputElement = document.createElement("input");
                     correctAnswerInput.type = "checkbox";
                     correctAnswerInput.checked = answer ? answer.isCorrect : false;
+
                     // Pridáme event listener na zmenu checkboxu pre označenie správnej odpovede
-                    correctAnswerInput.addEventListener("change", (e) => {
-                        newAnswer.isCorrect = e.target.checked;
+                    correctAnswerInput.addEventListener("change", (e: Event) => {
+                        newAnswer.isCorrect = (e.target as HTMLInputElement).checked;
                     });
+
                     // Vytvoríme label pre označenie správnej odpovede
-                    const correctAnswerLabel = document.createElement("label");
+                    const correctAnswerLabel: HTMLLabelElement = document.createElement("label");
                     correctAnswerLabel.textContent = "Correct answer";
+
                     // Pridáme všetky vytvorené prvky do odpovede
                     answerDiv.appendChild(answerInput);
-                    const correctAnswerDiv = document.createElement("div");
+
+                    const correctAnswerDiv: HTMLDivElement = document.createElement("div");
                     correctAnswerDiv.classList.add("correctAnswerDiv");
                     correctAnswerDiv.appendChild(correctAnswerLabel);
                     correctAnswerDiv.appendChild(correctAnswerInput);
                     answerDiv.appendChild(correctAnswerDiv);
+
                     answersDiv.appendChild(answerDiv);
+
                     // Pridáme novú odpoveď do zoznamu odpovedí pre otázku
                     question.answers[i] = newAnswer;
                 }
+
                 // Vytvoríme prázdny riadok a horizontálnu čiaru pre oddelenie
-                const brElement = document.createElement("br");
-                const hr = document.createElement("hr");
+                const brElement: HTMLBRElement = document.createElement("br");
+                const hr: HTMLHRElement = document.createElement("hr");
+
                 // Pridáme všetky vytvorené prvky do kontajnera pre otázky
                 questionDivContainer.appendChild(questionLabel);
                 questionDivContainer.appendChild(brElement);
@@ -182,14 +235,18 @@ if (testId != null) {
                 questionDivContainer.appendChild(buttonToDelete);
                 testUpdateQuestionsContainer.appendChild(questionDivContainer);
             }
+
             // Táto funkcia aktualizuje čísla otázok v zobrazení kvízových otázok.
-            function updateQuestionNumbers() {
+
+            function updateQuestionNumbers(): void {
                 // Získaj všetky kontajnery pre otázky v kvíze.
-                const questionContainers = testUpdateQuestionsContainer.querySelectorAll('.question');
+                const questionContainers: NodeListOf<HTMLDivElement> = testUpdateQuestionsContainer.querySelectorAll('.question');
+
                 // Prejdi všetky kontajnery pre otázky a aktualizuj ich čísla.
-                for (let i = 0; i < questionContainers.length; i++) {
+                for (let i: number = 0; i < questionContainers.length; i++) {
                     // Získaj referenciu na label element pre otázku.
-                    const label = questionContainers[i].querySelector('label');
+                    const label: HTMLLabelElement = <HTMLLabelElement>questionContainers[i].querySelector('label');
+
                     // Skontroluj, či bol label nájdený.
                     if (label) {
                         // Aktualizuj text labelu na základe aktuálnej pozície otázky.
@@ -197,12 +254,10 @@ if (testId != null) {
                     }
                 }
             }
-        }
-        else {
+        } else {
             window.location.href = "/admin/pages/allTests.html";
         }
     });
-}
-else {
+} else {
     window.location.href = "/admin/pages/allTests.html";
 }
